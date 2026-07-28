f19a8653dbedf9174970fa095516fe860dd73977
kannaniron1977-alt <kannaniron1977@gmail.com>
2026-07-28 16:23:50 +0530

    Add Main class with Swing UI initialization

---
+++ Main.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {}
+            new LoginFrame().setVisible(true);
+        });
+    }
+}
